446083c32905ca80e770fa3bf48a40b13abc303c
yaelroq04 <yael.roque04@outlook.com>
2026-01-07 15:04:05 -0800

    day 1 code added

---
+++ Unit 1/Output.cpp
@@ -0,0 +1,36 @@
+/*
+Yael Roque
+CST 116 - Programming Concepts I
+01/07/2026
+
+Pseudo Code
+main
+Output "Hello, World!" to the screen
+calls poem function
+end main
+
+poem function
+outputs Nothing Gold Can Stay
+end function
+*/
+
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+
+void Poem();
+
+int main() {
+    cout << "Hello, World!\n";
+    Poem();
+
+    return 0;
+}
+
+void Poem(){
+    cout << "Nature's first green is gold,\nHer hardest hue to hold.\nHer early leaf's a flower,\nBut only so an hour.";
+
+    return;
+}
